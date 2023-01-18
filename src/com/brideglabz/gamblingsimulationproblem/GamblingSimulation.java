845a267055113b31097f9ffc41a218c51066f120
drs317lakshami <devrajkumar317@gmail.com>
2023-01-19 00:13:02 +0530

    Added: start with a stake of 00 every day and bet every game

--- src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
@@ -6,6 +6,14 @@ public class GamblingSimulation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Gambling Simulation Problems");
-	
+		GamblingSimulation gambler = new GamblingSimulation();
+		gambler.gameInitialise();
+	}
+
+	public void gameInitialise() {
+		System.out.println("\nInitialising game...");
+		funds = 100;
+		bet = 1;
+		System.out.println("Funds : $" + funds);
 	}
 }
