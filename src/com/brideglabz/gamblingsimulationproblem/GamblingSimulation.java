cb47837fcebcbe87a45eb69b1a4272f3d0bcce55
drs317lakshami <devrajkumar317@gmail.com>
2023-01-19 00:07:56 +0530

    welcome to gamblining simulation logic

---
+++ src/com/brideglabz/gamblingsimulationproblem/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package com.brideglabz.gamblingsimulationproblem;
+
+public class GamblingSimulation {
+	public int funds = 0;
+	public int bet = 0;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Gambling Simulation Problems");
+	
+	}
+}
